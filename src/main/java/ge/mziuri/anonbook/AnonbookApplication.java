81daa61382716cdc442033def67e86ab52725eaa
HoniT <ioanebaidoshvili@gmail.com>
2026-05-27 12:31:20 +0400

    Init

---
+++ src/main/java/ge/mziuri/anonbook/AnonbookApplication.java
@@ -0,0 +1,13 @@
+package ge.mziuri.anonbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/mziuri/anonbook/AnonbookApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.mziuri.anonbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
